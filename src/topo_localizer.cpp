431496e71f1156c2e820cc7dd5267df890173551
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-06-02 11:23:44 +0900

    🐛fix: topomap画像サイズ修正

--- src/topo_localizer.cpp
+++ src/topo_localizer.cpp
@@ -72,11 +72,11 @@ void TopoLocalizer::loadMap(const std::string& map_path) {
 
 torch::Tensor TopoLocalizer::extractFeature(const cv::Mat& image) {
     cv::Mat resized;
-    cv::resize(image, resized, cv::Size(200, 88));
+    cv::resize(image, resized, cv::Size(88, 88));
     resized.convertTo(resized, CV_32FC3, 1.0 / 255.0);
 
     resized = resized.clone();  // ensure contiguous memory
-    auto input_tensor = torch::from_blob(resized.data, {1, 88, 200, 3}, torch::kFloat32)
+    auto input_tensor = torch::from_blob(resized.data, {1, 88, 88, 3}, torch::kFloat32)
                             .permute({0, 3, 1, 2})  // [N, C, H, W]
                             .clone()
                             .to(device_);
